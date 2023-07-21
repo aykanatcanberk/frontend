8970c6e210f2a602fb7210e160d20efb979b9d78
Tanem Çelik <tanemcelik@hotmail.com>
2023-07-21 10:31:58 +0300

    Update Advert.jsx
    İlan component son hali

--- src/components/Advert/Advert.jsx
+++ src/components/Advert/Advert.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import { styled } from '@mui/system';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
@@ -18,7 +18,40 @@ import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import Box from '@mui/material/Box';
 import BusinessIcon from '@mui/icons-material/Business';
 import { withTheme } from '@emotion/react';
+import TextareaAutosize from '@mui/base/TextareaAutosize';
+import { grey } from '@mui/material/colors';
+import PlaceIcon from '@mui/icons-material/Place';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 
+const StyledTextarea = styled(TextareaAutosize)(
+  ({ theme }) => `
+  width: 320px;
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+  font-weight: 400;
+  line-height: 1.5;
+  padding: 8px;
+  border-radius: 12px 12px 0 12px;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+  box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+
+  &:hover {
+    border-color: ${blue[400]};
+  }
+
+  &:focus {
+    border-color: ${blue[400]};
+    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[500] : blue[200]};
+  }
+
+  // firefox
+  &:focus-visible {
+    outline: 0;
+  }
+`,
+);
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
@@ -45,23 +78,26 @@ export default function RecipeReviewCard() {
   }
   title={
     <Typography variant="h8" fontWeight="bold">
-      İlan Başlığı
+      Yazılım Geliştirme Uzmanı
     </Typography>
   }
   subheader={
     <Box display="flex" alignItems="center">
-      <CalendarMonthIcon sx={{ fontSize: 18, marginRight: 1 }} />
-      <Typography variant="body2" color="text.secondary">
-        Tarih
-      </Typography>
-    </Box>
+      <CalendarMonthIcon sx={{ fontSize: 14, marginLeft:1 }} />
+      <Typography variant="body2" color="text.secondary">14.06.2023</Typography>
+      <PlaceIcon sx={{fontSize:14, marginLeft:1 }}/>
+      <Typography variant="body2" color="text.secondary">Ankara</Typography>
+      <BusinessCenterIcon sx={{fontSize:14, marginLeft:1 }}/>
+      <Typography variant="body2" color="text.secondary">Staj</Typography>
+    </Box>   
   }
 />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          İlan Detayları
-        </Typography>
-      </CardContent>
+<StyledTextarea
+        maxRows={4}
+        aria-label="maximum height"
+        placeholder="Maximum 100 rows"
+        defaultValue="Daha önce Java kullanmış en az 2 yıl tecrübeli, lisans diplomalı yazılım geliştirme uzmanı arıyoruz"
+      />
       <CardActions disableSpacing>
 
       </CardActions>
