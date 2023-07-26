79fe11ad8901de77006517d56504f0457537a12e
Tuğçe Öz <tugceoz214@gmail.com>
2023-07-26 22:19:01 +0300

    Squashed commit of the following:
    commit dd3e5bd9e83ebf1cde3640dc9d0dbf76313defd5
    Author: Tuğçe Öz <tugceoz214@gmail.com>
    Date:   Wed Jul 26 21:42:14 2023 +0300
    
        son

--- src/components/Advert/Advert.jsx
+++ src/components/Advert/Advert.jsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import { styled } from '@mui/system';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import Typography from '@mui/material/Typography';
-import { blue } from '@mui/material/colors';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import Box from '@mui/material/Box';
-import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import TextareaAutosize from '@mui/base/TextareaAutosize';
-import { grey } from '@mui/material/colors';
-import BusinessIcon from '@mui/icons-material/Business';
-import CardActions from '@mui/joy/CardActions';
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { styled } from "@mui/system";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import Typography from "@mui/material/Typography";
+import { blue } from "@mui/material/colors";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import Box from "@mui/material/Box";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import TextareaAutosize from "@mui/base/TextareaAutosize";
+import { grey } from "@mui/material/colors";
+import BusinessIcon from "@mui/icons-material/Business";
+import CardActions from "@mui/joy/CardActions";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 const StyledTextarea = styled(TextareaAutosize)(
   ({ theme }) => `
@@ -24,33 +24,41 @@ const StyledTextarea = styled(TextareaAutosize)(
   padding: 8px;
   margin-left:5px;
   border-radius: 12px 12px 0 12px;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-  box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+  box-shadow: 0px 2px 2px ${
+    theme.palette.mode === "dark" ? grey[900] : grey[50]
+  };
 
-`,
+`
 );
 
-export default function Advert({advert}){
+export default function Advert({ advert }) {
   return (
     <Card sx={{ maxWidth: 380 }}>
       <CardHeader
         avatar={
-          <BusinessIcon sx={{ fontSize: 35, bgcolor: blue[500] }} aria-label="recipe" />
+          <BusinessIcon
+            sx={{ fontSize: 35, bgcolor: blue[500] }}
+            aria-label="recipe"
+          />
         }
         title={
           <Typography variant="h8" fontWeight="bold">
-             {advert.ilan_adi}
+            {advert.ilan_adi}
           </Typography>
         }
         subheader={
           <Box display="flex" alignItems="center">
             <CalendarMonthIcon sx={{ fontSize: 14, marginLeft: 1 }} />
-            <Typography variant="body2" color="text.secondary">{advert.ilan_date}
+            <Typography variant="body2" color="text.secondary">
+              {advert.ilan_date}
             </Typography>
             <BusinessCenterIcon sx={{ fontSize: 14, marginLeft: 1 }} />
-            <Typography variant="body2" color="text.secondary">{advert.ilan_tipi}</Typography>
+            <Typography variant="body2" color="text.secondary">
+              {advert.ilan_tipi}
+            </Typography>
           </Box>
         }
       />
@@ -62,11 +70,11 @@ export default function Advert({advert}){
         readOnly
       />
       <CardActions>
-      <Link to="/kurumsal-ilan">
-      <Button variant="solid" color="primary">
-            İlan Detayı 
+        <Link to="/kurumsal-ilan">
+          <Button variant="solid" color="primary" sx={{ marginLeft: 27 }}>
+            İlan Detayı
           </Button>
-          </Link>
+        </Link>
       </CardActions>
     </Card>
   );
