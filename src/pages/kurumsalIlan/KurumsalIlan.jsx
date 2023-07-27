2d9616077f7587ff92d69c0ab0ecc4c5e1d9c78a
Tuğçe Öz <tugceoz214@gmail.com>
2023-07-27 14:48:19 +0300

    kurumsal-ilan detay

--- src/pages/kurumsalIlan/KurumsalIlan.jsx
+++ src/pages/kurumsalIlan/KurumsalIlan.jsx
@@ -1,30 +1,64 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import İlanDetay from "../../components/ilanDetay/İlanDetay";
+import Advert from "../../components/Advert/Advert";
+import Typography from "@mui/joy/Typography";
 import db from "../../data/db.json";
 
 function KurumsalIlan() {
   return (
-    <Grid item xs={12} lg={9}>
+    <Grid container spacing={0} sx={{ marginTop: "60px" }}>
       <Grid
-        container
-        justifyContent="center"
-        alignItems="center"
-        marginTop={3}
-        spacing={2}
+        item
+        lg={7}
+        md={10}
+        sx={{ paddingRight: "100px", paddingLeft: "60px" }}
       >
-        {db["advert-detail"].map((advert, index) => (
-          <Grid
-            item
-            key={advert.id}
-            xs={12}
-            sm={12}
-            marginTop={10}
-            marginLeft={20}
-          >
-            <İlanDetay advert={advert} />
-          </Grid>
-        ))}
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          marginTop={3}
+          spacing={2}
+        >
+          {db["advert-detail"].map((advert, index) => (
+            <Grid
+              item
+              key={advert.id}
+              xs={12}
+              sm={12}
+              marginTop={10}
+              marginLeft={10}
+            >
+              <İlanDetay advert={advert} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        lg={4}
+        md={12}
+        sx={{
+          paddingRight: "150px",
+          "@media (max-width: 1280px)": {
+            textAlign: "center",
+            paddingRight: "5px",
+          },
+        }}
+      >
+        <Typography
+          level="h6"
+          className="Typography"
+          style={{ marginLeft: 2, marginTop: 10 }}
+        >
+          BAŞVURANLAR
+        </Typography>
+        <div style={{ marginTop: 20, marginRight: 10 }}>
+          {db.advert.map((advert) => (
+            <Advert key={advert.id} advert={advert} />
+          ))}
+        </div>
       </Grid>
     </Grid>
   );
