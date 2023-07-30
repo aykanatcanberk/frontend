0989da01d92ff573f78a0681985979583c518391
Tuğçe Öz <tugceoz214@gmail.com>
2023-07-30 18:46:28 +0300

    logo düzeltildi

--- src/components/tkpEdilenFirmalar/TakipEdilenFirmalar.jsx
+++ src/components/tkpEdilenFirmalar/TakipEdilenFirmalar.jsx
@@ -2,12 +2,20 @@
 Last Update: 25.07.2023 */
 
 import React from "react";
-import { Grid, Paper, Typography, Button, Card, CardActionArea, CardContent } from "@mui/material";
+import {
+  Grid,
+  Paper,
+  Typography,
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+} from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import { styled } from "@mui/material/styles";
 import aselsanlogo from "./images/aselsanlogo.png";
 import havelsanlogo from "./images/havelsanlogo.png";
-import roketsan from "./images/roketsan.png";
+import roketsanlogo from "./images/roketsanlogo.jpg";
 import tusas from "./images/tusas.png";
 import tet from "./images/tr-egitim-ve-teknoloji-as.png";
 
@@ -45,103 +53,103 @@ function TakipEdilenFirmalar() {
         Öne Çıkan Firmalar
       </Typography>
       <Grid container spacing={1}>
-      <Grid item xs={6} sm={6} md={6}>
-      <Card elevation={0} style={{borderRadius: "50%"}}>
-        <CardActionArea onClick={handleClick} href="/bireysel-firma/3">
-          {/* Karta eklemek istediğiniz içerik burada olacak */}
-          <SmallAvatarWrapper src={roketsan} alt="Roketsan" />
-          <CardContent>
-            <Typography
-              style={{
-                fontFamily: "Arial",
-                fontSize: "12px",
-                fontWeight: "normal",
-              }}
-            >
-              Roketsan
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
-    <Grid item xs={6} sm={6} md={6}>
-      <Card elevation={0} style={{borderRadius: "50%"}}>
-        <CardActionArea onClick={handleClick} href="/bireysel-firma/2">
-          {/* Karta eklemek istediğiniz içerik burada olacak */}
-          <SmallAvatarWrapper src={havelsanlogo} alt="Havelsan" />
-          <CardContent>
-            <Typography
-              style={{
-                fontFamily: "Arial",
-                fontSize: "12px",
-                fontWeight: "normal",
-              }}
-            >
-              Havelsan
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
-    <Grid item xs={6} sm={6} md={6}>
-      <Card elevation={0} style={{borderRadius: "50%"}}>
-        <CardActionArea onClick={handleClick} href="/bireysel-firma/1">
-          {/* Karta eklemek istediğiniz içerik burada olacak */}
-          <SmallAvatarWrapper src={aselsanlogo} alt="Aselsan" />
-          <CardContent>
-            <Typography
-              style={{
-                fontFamily: "Arial",
-                fontSize: "12px",
-                fontWeight: "normal",
-              }}
-            >
-              Aselsan
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
-    <Grid item xs={6} sm={6} md={6}>
-      <Card elevation={0} style={{borderRadius: "50%"}}>
-        <CardActionArea onClick={handleClick} href="/bireysel-firma/5">
-          {/* Karta eklemek istediğiniz içerik burada olacak */}
-          <SmallAvatarWrapper src={tusas} alt="TUSAŞ" />
-          <CardContent>
-            <Typography
-              style={{
-                fontFamily: "Arial",
-                fontSize: "12px",
-                fontWeight: "normal",
-              }}
-            >
-              TUSAŞ
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
+        <Grid item xs={6} sm={6} md={6}>
+          <Card elevation={0} style={{ borderRadius: "50%" }}>
+            <CardActionArea onClick={handleClick} href="/bireysel-firma/3">
+              {/* Karta eklemek istediğiniz içerik burada olacak */}
+              <SmallAvatarWrapper src={roketsanlogo} alt="Roketsan" />
+              <CardContent>
+                <Typography
+                  style={{
+                    fontFamily: "Arial",
+                    fontSize: "12px",
+                    fontWeight: "normal",
+                  }}
+                >
+                  Roketsan
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={6} sm={6} md={6}>
+          <Card elevation={0} style={{ borderRadius: "50%" }}>
+            <CardActionArea onClick={handleClick} href="/bireysel-firma/2">
+              {/* Karta eklemek istediğiniz içerik burada olacak */}
+              <SmallAvatarWrapper src={havelsanlogo} alt="Havelsan" />
+              <CardContent>
+                <Typography
+                  style={{
+                    fontFamily: "Arial",
+                    fontSize: "12px",
+                    fontWeight: "normal",
+                  }}
+                >
+                  Havelsan
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={6} sm={6} md={6}>
+          <Card elevation={0} style={{ borderRadius: "50%" }}>
+            <CardActionArea onClick={handleClick} href="/bireysel-firma/1">
+              {/* Karta eklemek istediğiniz içerik burada olacak */}
+              <SmallAvatarWrapper src={aselsanlogo} alt="Aselsan" />
+              <CardContent>
+                <Typography
+                  style={{
+                    fontFamily: "Arial",
+                    fontSize: "12px",
+                    fontWeight: "normal",
+                  }}
+                >
+                  Aselsan
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={6} sm={6} md={6}>
+          <Card elevation={0} style={{ borderRadius: "50%" }}>
+            <CardActionArea onClick={handleClick} href="/bireysel-firma/5">
+              {/* Karta eklemek istediğiniz içerik burada olacak */}
+              <SmallAvatarWrapper src={tusas} alt="TUSAŞ" />
+              <CardContent>
+                <Typography
+                  style={{
+                    fontFamily: "Arial",
+                    fontSize: "12px",
+                    fontWeight: "normal",
+                  }}
+                >
+                  TUSAŞ
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
         <Grid item xs={12} sm={12} md={12}>
           <Grid container justifyContent="center" spacing={1}>
-          <Grid item xs={6} sm={6} md={6}>
-      <Card elevation={0} style={{borderRadius: "50%"}}>
-        <CardActionArea onClick={handleClick} href="/bireysel-firma/4">
-          {/* Karta eklemek istediğiniz içerik burada olacak */}
-          <SmallAvatarWrapper src={tet} alt="TET A.Ş." />
-          <CardContent>
-            <Typography
-              style={{
-                fontFamily: "Arial",
-                fontSize: "12px",
-                fontWeight: "normal",
-              }}
-            >
-              TET A.Ş.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
+            <Grid item xs={6} sm={6} md={6}>
+              <Card elevation={0} style={{ borderRadius: "50%" }}>
+                <CardActionArea onClick={handleClick} href="/bireysel-firma/4">
+                  {/* Karta eklemek istediğiniz içerik burada olacak */}
+                  <SmallAvatarWrapper src={tet} alt="TET A.Ş." />
+                  <CardContent>
+                    <Typography
+                      style={{
+                        fontFamily: "Arial",
+                        fontSize: "12px",
+                        fontWeight: "normal",
+                      }}
+                    >
+                      TET A.Ş.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
@@ -158,8 +166,8 @@ function TakipEdilenFirmalar() {
           borderColor: "blue",
           backgroundColor: "transparent",
           "&:hover": {
-          backgroundColor: "transparent",
-        },
+            backgroundColor: "transparent",
+          },
         }}
       >
         Tüm Firmaları Görüntüle
