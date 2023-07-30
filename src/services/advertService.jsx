f94138b290bc6519785225db7d7792880d1c5cab
Tanem Çelik <tanemcelik@hotmail.com>
2023-07-30 20:48:56 +0300

    advertService get eklenmesi ama hala json adresle

--- src/services/advertService.jsx
+++ src/services/advertService.jsx
@@ -1,7 +1,9 @@
-import axios from 'axios';
-const addAdvert = (newAdvert) => {
-    const apiUrl = 'http://localhost:3000/kurumsal-ilanlar'; 
-    return axios.post(apiUrl, newAdvert);
+import axios from "axios";
+const apiUrl = 'http://localhost:3000/kurumsal-ilanlar'; 
+export const addAdvert = (newAdvert) => {
+return axios.post(apiUrl, newAdvert);
   };
-  
-  export default addAdvert;
+
+export const getAdvert = () => {
+  return axios.get(apiUrl);
+};
