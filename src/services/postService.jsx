77d283744323a35a6496df5bd680eb4e5e46dd3f
Tuğçe Öz <tugceoz214@gmail.com>
2023-07-31 01:52:20 +0300

    service

--- src/services/postService.jsx
+++ src/services/postService.jsx
@@ -1,7 +1,5 @@
 import axios from "axios";
-const addPost = (newPost) => {
-  const apiUrl = "http://localhost:3000/kurumsal-anasayfa";
-  return axios.post(apiUrl, newPost);
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+export const addPost = (newPost) => {
+  return axios.post("${BASE_URL}/kurumsal-anasayfa", newPost);
 };
-
-export default addPost;
