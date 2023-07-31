c696681c601cdbb834c0330a65858cd7cfa944ab
İrem CAKCAK <83467278+iremcakcak5@users.noreply.github.com>
2023-07-31 23:29:02 +0300

    bireysel firmalar

--- src/pages/bireyselFirmalar/BireyselFirmalar.jsx
+++ src/pages/bireyselFirmalar/BireyselFirmalar.jsx
@@ -1,37 +1,17 @@
 import * as React from "react";
 import FirmaKartvizit from "../../components/firmaKartvizit/FirmaKartvizit";
-import { Container, InputAdornment, TextField } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import { useState } from "react";
+import { Container} from "@mui/material";
 import "./BireyselFirmalar.css";
 import db from "../../data/db.json";
-
+import SearchBar from "../../components/searchBar/searchBar"
 export default function ComplexGrid() {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
+ 
   return (
     <>
       <Container maxWidth="md" sx={{ mt: 8 }}>
-        <TextField
-          id="search"
-          type="search"
-          label="Search"
-          value={searchTerm}
-          onChange={handleChange}
-          sx={{ width: 800 }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
+   <SearchBar/>
       </Container>
-      <div className="searchFilter">{}</div>
+
       <div className="firmaContainer">
         {db["info-firma"].map((companycard) => (
           <FirmaKartvizit key={companycard.id} companycard={companycard} />
