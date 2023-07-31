8280586fd3ae7a04bc6899ac13e29b641ee25056
Tanem Çelik <tanemcelik@hotmail.com>
2023-07-31 15:25:10 +0300

    add advert düzeltme

--- src/services/advertService.jsx
+++ src/services/advertService.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const addAdvert = (newAdvert) => {
-  return axios.post(`${BASE_URL}/kurumsal-ilanlar`);
+  return axios.post(`${BASE_URL}/kurumsal-ilanlar`,newAdvert);
 };
 
 export const getAdverts = () => {
