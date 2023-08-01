780a86aabd094e9ad9d7f12c8df846c78d9b3ffb
Tuğçe Öz <tugceoz214@gmail.com>
2023-08-01 11:39:27 +0300

    search bar aktifleşti

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,31 +1,27 @@
-import React from 'react';
+import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import { useState } from "react";
 import { InputAdornment, TextField } from "@mui/material";
 
-function SearchBar() {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
+function SearchBar({ searchTerm, onChange }) {
   return (
-    <div><TextField
-          id="search"
-          type="search"
-          label="Search"
-          value={searchTerm}
-          onChange={handleChange}
-          sx={{ width: 800 }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        /></div>
-  )
+    <div>
+      <TextField
+        id="search"
+        type="search"
+        label="Search"
+        value={searchTerm}
+        onChange={onChange}
+        sx={{ width: 800 }}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </div>
+  );
 }
 
 export default SearchBar;
--- src/pages/bireyselFirmalar/BireyselFirmalar.jsx
+++ src/pages/bireyselFirmalar/BireyselFirmalar.jsx
@@ -1,19 +1,30 @@
-import * as React from "react";
+import React, { useState } from "react";
 import FirmaKartvizit from "../../components/firmaKartvizit/FirmaKartvizit";
-import { Container} from "@mui/material";
+import { Container } from "@mui/material";
 import "./BireyselFirmalar.css";
 import db from "../../data/db.json";
-import SearchBar from "../../components/searchBar/searchBar"
+import SearchBar from "../../components/searchBar/searchBar";
+
 export default function ComplexGrid() {
- 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // "searchTerm" kullanarak firmaları filtreleyin
+  const filteredCompanies = db["info-firma"].filter((company) =>
+    company.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
       <Container maxWidth="md" sx={{ mt: 8 }}>
-   <SearchBar/>
+        <SearchBar searchTerm={searchTerm} onChange={handleChange} />
       </Container>
 
       <div className="firmaContainer">
-        {db["info-firma"].map((companycard) => (
+        {filteredCompanies.map((companycard) => (
           <FirmaKartvizit key={companycard.id} companycard={companycard} />
         ))}
       </div>
