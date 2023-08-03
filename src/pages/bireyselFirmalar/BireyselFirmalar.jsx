d40cad6fbffc846bbf1623af506bfc045416cd82
Tanem Çelik <tanemcelik@hotmail.com>
2023-08-03 18:18:17 +0300

    backend bireysel firmalar

--- src/pages/bireyselFirmalar/BireyselFirmalar.jsx
+++ src/pages/bireyselFirmalar/BireyselFirmalar.jsx
@@ -16,7 +16,7 @@ export default function ComplexGrid() {
     company.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 useEffect(()=>{
-  const token="eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoidGFuZW1AZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiUGVyc29uIiwiZXhwIjoxNjkxMDc0MTcwfQ.n0_Uxexo8q5OJWx3VRgceqoTppgn9MBDHQHAXCv8BVE"
+  const token=localStorage.getItem("token");
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
   axios.get(`https://localhost:7029/api/Get_AllCompnay/get all company`).then((response)=>{
     setFirmalar(response.data)
