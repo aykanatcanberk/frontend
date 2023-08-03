be0590b3f5f986a8f7acb448c6e8ad7024d98732
H.B. Bilge <106149124+HasanBasriBILGE@users.noreply.github.com>
2023-08-03 21:16:46 +0300

    Bireysel-KurumsalAnasayfa

--- src/components/bryselAsayfaProfilComp/profilComp.jsx
+++ src/components/bryselAsayfaProfilComp/profilComp.jsx
@@ -1,11 +1,13 @@
 /* Author: Hasan Basri BİLGE
 Last Update: 25.07.2023 */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Paper, Typography } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import { styled } from "@mui/material/styles";
 import pp from "./images/pp";
+import { getUserEduInfo, getUserInfo } from "../../services/userService";
+import NotFoundError from "../../routes/NotFoundError";
 
 const CardWrapper = styled(Paper)({
   padding: "1rem",
@@ -22,35 +24,62 @@ const AvatarWrapper = styled(Avatar)({
 });
 
 function ProfilComp() {
+  const [eduinf, setCompanyDataList] = useState([]);
+
+  useEffect(() => {
+    getUserEduInfo()
+      .then((response) => {
+        const eduinfo = response.data;
+        setCompanyDataList(eduinfo);
+      })
+      .catch(() => {
+        return <NotFoundError props={"Böyle bir edu bilgisi mevcut değil."} />;
+      });
+  }, []);
+
+  const [userinfo, setUserData] = useState([]);
+
+  useEffect(() => {
+    getUserInfo()
+      .then((response) => {
+        const userinf = response.data;
+        setUserData(userinf);
+      })
+      .catch(() => {
+        return (
+          <NotFoundError props={"Böyle bir kullanıcı bilgisi mevcut değil."} />
+        );
+      });
+  }, []);
   return (
     <CardWrapper>
-          <AvatarWrapper src={pp} alt="Profil Avatarı" />
-          <Typography
-            variant="h6"
-            style={{
-              fontFamily: "Arial",
-              fontSize: "14px",
-              fontWeight: "normal",
-            }}
-          >
-            Patrick Bateman
-          </Typography>
-          <Grid item xs={12} sm={12} md={12}>
-            <Typography
-              variant="subtitle1"
-              style={{
-                fontFamily: "Arial",
-                fontSize: "12px",
-                fontWeight: "normal",
-                color: "secondary",
-                textAlign: "center",
-              }}
-            >
-              Öğrenci
-            </Typography>
-          </Grid>
-        </CardWrapper>
-  )
+      <AvatarWrapper src={pp} alt="Profil Avatarı" />
+      <Typography
+        variant="h6"
+        style={{
+          fontFamily: "Arial",
+          fontSize: "14px",
+          fontWeight: "normal",
+        }}
+      >
+        {userinfo.name} <span /> {userinfo.surname}
+      </Typography>
+      <Grid item xs={12} sm={12} md={12}>
+        <Typography
+          variant="subtitle1"
+          style={{
+            fontFamily: "Arial",
+            fontSize: "12px",
+            fontWeight: "normal",
+            color: "secondary",
+            textAlign: "center",
+          }}
+        >
+          {eduinf.departmentName}
+        </Typography>
+      </Grid>
+    </CardWrapper>
+  );
 }
 
 export default ProfilComp;
